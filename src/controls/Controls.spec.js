1769faea522149adf080891f8b95133f82f9963b
Andrew Wilson <andrew.wilson90@hotmail.com>
2019-11-11 16:20:34 -0600

    finish MVP

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,39 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+
+import Controls from './Controls';
+
+test('Controls renders correctly', () => {
+  expect(render(<Controls />)).toMatchSnapshot();
+})
+
+test('toggleClosed is called on click', () => {
+  const toggleClosedMock = jest.fn();
+  const { getByText } = render(<Controls toggleClosed={toggleClosedMock} />);
+  const toggleButton = getByText(/close gate/i);
+  fireEvent.click(toggleButton);
+  expect(toggleClosedMock).toHaveBeenCalled();
+  expect(toggleClosedMock).toHaveBeenCalledTimes(1);
+})
+
+test('toggleLocked is called on click', () => {
+  const toggleLockedMock = jest.fn();
+  const { getByText } = render(<Controls closed={true} toggleLocked={toggleLockedMock} />);
+  const toggleButton = getByText(/lock gate/i);
+  fireEvent.click(toggleButton);
+  expect(toggleLockedMock).toHaveBeenCalled();
+  expect(toggleLockedMock).toHaveBeenCalledTimes(1);
+})
+
+test('closed toggle button is disabled if the gate is locked', () => {
+  const { getByText } = render(<Controls locked={true} closed={true} />);
+  const toggleButton = getByText(/open gate/i);
+  expect(toggleButton.disabled).toEqual(true);
+})
+
+test('lock toggle button is disabled if the gate is open', () => {
+  const { getByText } = render(<Controls />);
+  const toggleButton = getByText(/lock gate/i);
+  expect(toggleButton.disabled).toEqual(true);
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,40 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+
+import Display from './Display';
+
+test('Display renders correctly', () => {
+  expect(render(<Display />)).toMatchSnapshot();
+})
+
+test('it can mock', () => {
+  const mock = jest.fn();
+  mock('smile');
+  expect(mock).toHaveBeenCalled();
+})
+
+test('displays closed with red-led if the closed prop is true', () => {
+  const { queryByText, getByText } = render(<Display closed={true} />);
+  getByText(/closed/i);
+  queryByText(/red-led/i)
+})
+
+test('displays locked with red-led if the locked prop is true', () => {
+  const { queryByText, getByText } = render(<Display locked={true} />);
+  getByText(/locked/i);
+  queryByText(/red-led/i)
+})
+
+
+test('displays open with green-led if the closed prop is false', () => {
+  const { queryByText, getByText } = render(<Display closed={false} />);
+  getByText(/open/i);
+  queryByText(/green-led/i)
+})
+
+test('displays unlocked with green-led if the locked prop is false', () => {
+  const { queryByText, getByText } = render(<Display locked={false} />);
+  getByText(/unlocked/i);
+  queryByText(/green-led/i)
+})
